36f3762ffe2c23ff5869130186cfc0082bf88eda
cr0sh <ska827@naver.com>
2015-10-28 23:59:41 +0900

    Logger uses Println format(...interfaces{})

--- src/util/logging/logging.go
+++ src/util/logging/logging.go
@@ -8,36 +8,37 @@ import (
 //Level declares how much data will be printed to console. 0: debugging, 1: verbose, 2: warning, 3: error, 4: nothing(only panic)
 var Level int
 
-func log(str string, level int) {
+func log(level int, prefix string, f ...interface{}) {
 	if level < Level {
 		return
 	}
 	t := time.Now()
 	hour, min, sec := t.Clock()
-	fmt.Printf("[%02d:%02d:%02d] %s\n", hour, min, sec, str)
+	fmt.Printf("[%02d:%02d:%02d] %s", hour, min, sec, prefix)
+	fmt.Println(f...)
 }
 
 //Debug prints DEBUG-level log
-func Debug(str string) {
-	log("DEBUG: "+str, 0)
+func Debug(f ...interface{}) {
+	log(0, "DEBUG: ", f...)
 }
 
 //Verbose prints INFO-level log
-func Verbose(str string) {
-	log("INFO: "+str, 1)
+func Verbose(f ...interface{}) {
+	log(1, "INFO: ", f...)
 }
 
 //Warning prints WARN-level log
-func Warning(str string) {
-	log("WARN: "+str, 2)
+func Warning(f ...interface{}) {
+	log(2, "WARN: ", f...)
 }
 
 //FromError prints ERROR-level log from error
 func FromError(err error, level int) {
-	log("ERROR: "+err.Error(), level)
+	log(level, "ERROR:", err.Error())
 }
 
 //Error prints ERROR-level log from given string
-func Error(str string) {
-	log("ERROR: "+str, 3)
+func Error(f ...interface{}) {
+	log(3, "ERROR: ", f...)
 }
